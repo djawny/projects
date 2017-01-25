7edad4b8341e25a2ffa3565c52a7804a1ff80d67
djawny <djawny@op.pl>
2017-01-25 19:46:22 +0100

    zmiana 25/01

--- src/main/java/sda/zadanie22_json_unirest/UnirestMapper.java
+++ src/main/java/sda/zadanie22_json_unirest/UnirestMapper.java
@@ -3,11 +3,12 @@ package sda.zadanie22_json_unirest;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.mashape.unirest.http.ObjectMapper;
 import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 
 import java.io.IOException;
 
 public class UnirestMapper {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnirestException {
         Unirest.setObjectMapper(new ObjectMapper() {
             private com.fasterxml.jackson.databind.ObjectMapper jacksonObjectMapper
                     = new com.fasterxml.jackson.databind.ObjectMapper();
@@ -28,5 +29,8 @@ public class UnirestMapper {
                 }
             }
         });
+
+        Person person = Unirest.get("http://192.168.2.17:8080/customers/dj01").asObject(Person.class).getBody();
+        System.out.println(person);
     }
 }
