5c407fd5fc83e66188e5e73d66e7d9b948534d12
djawny <djawny@op.pl>
2017-01-25 17:34:55 +0100

    zmiana 25/01

---
+++ src/main/java/zadanie19_json/zad2/Holiday.java
@@ -0,0 +1,58 @@
+package zadanie19_json.zad2;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Holiday {
+    private String name;
+    //    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+//    private Date date;
+    private String date;
+    //    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+//    private Date observed;
+    private String observed;
+    @JsonProperty("public")
+    private Boolean isPublic;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getObserved() {
+        return observed;
+    }
+
+    public void setObserved(String observed) {
+        this.observed = observed;
+    }
+
+    public Boolean getPublic() {
+        return isPublic;
+    }
+
+    public void setPublic(Boolean aPublic) {
+        isPublic = aPublic;
+    }
+
+    @Override
+    public String toString() {
+        return "Holiday{" +
+                "name='" + name + '\'' +
+                ", date=" + date +
+                ", observed=" + observed +
+                ", isPublic=" + isPublic +
+                '}';
+    }
+}
---
+++ src/main/java/zadanie19_json/zad2/Main.java
@@ -0,0 +1,126 @@
+package zadanie19_json.zad2;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String json = "{\n" +
+                "\n" +
+                "    \"status\": 200,\n" +
+                "    \"holidays\": {\n" +
+                "        \"2016-01-01\": [\n" +
+                "            {\n" +
+                "                \"name\": \"Nowy Rok\",\n" +
+                "                \"date\": \"2016-01-01\",\n" +
+                "                \"observed\": \"2016-01-01\",\n" +
+                "                \"public\": true\n" +
+                "            }\n" +
+                "        ],\n" +
+                "        \"2016-01-06\": [\n" +
+                "            {\n" +
+                "                \"name\": \"Święto Trzech Króli\",\n" +
+                "                \"date\": \"2016-01-06\",\n" +
+                "                \"observed\": \"2016-01-06\",\n" +
+                "                \"public\": true\n" +
+                "            }\n" +
+                "        ],\n" +
+                "        \"2016-03-27\": [\n" +
+                "            {\n" +
+                "                \"name\": \"Niedziela Wielkanocna\",\n" +
+                "                \"date\": \"2016-03-27\",\n" +
+                "                \"observed\": \"2016-03-27\",\n" +
+                "                \"public\": true\n" +
+                "            }\n" +
+                "        ],\n" +
+                "        \"2016-03-28\": [\n" +
+                "            {\n" +
+                "                \"name\": \"Poniedziałek Wielkanocny\",\n" +
+                "                \"date\": \"2016-03-28\",\n" +
+                "                \"observed\": \"2016-03-28\",\n" +
+                "                \"public\": true\n" +
+                "            }\n" +
+                "        ],\n" +
+                "        \"2016-05-01\": [\n" +
+                "            {\n" +
+                "                \"name\": \"Święto Pracy\",\n" +
+                "                \"date\": \"2016-05-01\",\n" +
+                "                \"observed\": \"2016-05-01\",\n" +
+                "                \"public\": true\n" +
+                "            }\n" +
+                "        ],\n" +
+                "        \"2016-05-03\": [\n" +
+                "            {\n" +
+                "                \"name\": \"Święto Narodowe Trzeciego Maja\",\n" +
+                "                \"date\": \"2016-05-03\",\n" +
+                "                \"observed\": \"2016-05-03\",\n" +
+                "                \"public\": true\n" +
+                "            }\n" +
+                "        ],\n" +
+                "        \"2016-05-15\": [\n" +
+                "            {\n" +
+                "                \"name\": \"Zesłanie Ducha Świętego\",\n" +
+                "                \"date\": \"2016-05-15\",\n" +
+                "                \"observed\": \"2016-05-15\",\n" +
+                "                \"public\": true\n" +
+                "            }\n" +
+                "        ],\n" +
+                "        \"2016-05-26\": [\n" +
+                "            {\n" +
+                "                \"name\": \"Dzień Bożego Ciała\",\n" +
+                "                \"date\": \"2016-05-26\",\n" +
+                "                \"observed\": \"2016-05-26\",\n" +
+                "                \"public\": true\n" +
+                "            }\n" +
+                "        ],\n" +
+                "        \"2016-08-15\": [\n" +
+                "            {\n" +
+                "                \"name\": \"Wniebowzięcie Najświętszej Maryi Panny\",\n" +
+                "                \"date\": \"2016-08-15\",\n" +
+                "                \"observed\": \"2016-08-15\",\n" +
+                "                \"public\": true\n" +
+                "            }\n" +
+                "        ],\n" +
+                "        \"2016-11-01\": [\n" +
+                "            {\n" +
+                "                \"name\": \"Uroczystość Wszystkich Świętych\",\n" +
+                "                \"date\": \"2016-11-01\",\n" +
+                "                \"observed\": \"2016-11-01\",\n" +
+                "                \"public\": true\n" +
+                "            }\n" +
+                "        ],\n" +
+                "        \"2016-11-11\": [\n" +
+                "            {\n" +
+                "                \"name\": \"Narodowe Święto Niepodległości\",\n" +
+                "                \"date\": \"2016-11-11\",\n" +
+                "                \"observed\": \"2016-11-11\",\n" +
+                "                \"public\": true\n" +
+                "            }\n" +
+                "        ],\n" +
+                "        \"2016-12-25\": [\n" +
+                "            {\n" +
+                "                \"name\": \"Pierwszy dzień Bożego Narodzenia\",\n" +
+                "                \"date\": \"2016-12-25\",\n" +
+                "                \"observed\": \"2016-12-25\",\n" +
+                "                \"public\": true\n" +
+                "            }\n" +
+                "        ],\n" +
+                "        \"2016-12-26\": [\n" +
+                "            {\n" +
+                "                \"name\": \"Drugi dzień Bożego Narodzenia\",\n" +
+                "                \"date\": \"2016-12-26\",\n" +
+                "                \"observed\": \"2016-12-26\",\n" +
+                "                \"public\": true\n" +
+                "            }\n" +
+                "        ]\n" +
+                "    }\n" +
+                "\n" +
+                "}";
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        HolidaysCalendar holidaysCalendar = objectMapper.readValue(json, HolidaysCalendar.class);
+
+        System.out.println(holidaysCalendar);
+    }
+}
