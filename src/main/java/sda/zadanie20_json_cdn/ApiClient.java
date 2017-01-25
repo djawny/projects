e978dc9ffd78b36a95feb3e780f719f6582eccff
djawny <djawny@op.pl>
2017-01-25 18:13:07 +0100

    zmiana 25/01

--- src/main/java/sda/zadanie20_json_cdn/ApiClient.java
+++ src/main/java/sda/zadanie20_json_cdn/ApiClient.java
@@ -1,10 +1,12 @@
 package sda.zadanie20_json_cdn;
 
 import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class ApiClient {
-    public static void main(String[] args) {
-        Unirest.get("http://192.168.2.17:8080/customers/");
+    public static void main(String[] args) throws UnirestException {
+        String response = Unirest.get("http://192.168.2.17:8080/customers/").asString().getBody();
+        System.out.println(response);
 
     }
 }
