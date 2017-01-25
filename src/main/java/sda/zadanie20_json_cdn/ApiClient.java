1a9b0dcf01ad7a175af0efe56276f1b72cd21e74
djawny <djawny@op.pl>
2017-01-25 19:18:34 +0100

    zmiana 25/01

--- src/main/java/sda/zadanie20_json_cdn/ApiClient.java
+++ src/main/java/sda/zadanie20_json_cdn/ApiClient.java
@@ -9,17 +9,17 @@ public class ApiClient {
     public static void main(String[] args) throws UnirestException, IOException {
 //        String response = Unirest.get("http://192.168.2.17:8080/customers/123456").asString().getBody();
 //        System.out.println(response);
-        String json = "    {\n" +
-                "        \"firstName\": \"Adam1111111\",\n" +
-                "        \"lastName\": \"Mickiewicz22222222\",\n" +
-                "        \"birthYear\": 1798,\n" +
-                "        \"height\": 1.7,\n" +
-                "        \"id\": \"sdfsdf\"\n" +
-                "    }";
-        String postResponse = Unirest.post("http://192.168.2.17:8080/customers/dj01")
-                .header("Content-Type", "application/json").body(json)
-                .asString().getBody();
-        System.out.println(postResponse);
+//        String json = "    {\n" +
+//                "        \"firstName\": \"Adam1111111\",\n" +
+//                "        \"lastName\": \"Mickiewicz22222222\",\n" +
+//                "        \"birthYear\": 1798,\n" +
+//                "        \"height\": 1.7,\n" +
+//                "        \"id\": \"sdfsdf\"\n" +
+//                "    }";
+//        String postResponse = Unirest.post("http://192.168.2.17:8080/customers/dj01")
+//                .header("Content-Type", "application/json").body(json)
+//                .asString().getBody();
+//        System.out.println(postResponse);
 
     }
 }
---
+++ src/main/java/sda/zadanie20_json_cdn/Person.java
@@ -0,0 +1,49 @@
+package sda.zadanie20_json_cdn;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int birthYear;
+    private double height;
+    private String id;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
