48f8a1f35d968d2ef6afb2346561efed3344a3c7
djawny <djawny@op.pl>
2017-01-25 19:09:29 +0100

    zmiana 25/01

--- src/main/java/sda/zadanie20_json_cdn/ApiClient.java
+++ src/main/java/sda/zadanie20_json_cdn/ApiClient.java
@@ -7,9 +7,19 @@ import java.io.IOException;
 
 public class ApiClient {
     public static void main(String[] args) throws UnirestException, IOException {
-        String response = Unirest.get("http://192.168.2.17:8080/customers/").asString().getBody();
-        System.out.println(response);
-
+//        String response = Unirest.get("http://192.168.2.17:8080/customers/123456").asString().getBody();
+//        System.out.println(response);
+        String json = "    {\n" +
+                "        \"firstName\": \"Adam1111111\",\n" +
+                "        \"lastName\": \"Mickiewicz22222222\",\n" +
+                "        \"birthYear\": 1798,\n" +
+                "        \"height\": 1.7,\n" +
+                "        \"id\": \"sdfsdf\"\n" +
+                "    }";
+        String postResponse = Unirest.post("http://192.168.2.17:8080/customers/dj01")
+                .header("Content-Type", "application/json").body(json)
+                .asString().getBody();
+        System.out.println(postResponse);
 
     }
 }
