b35846c901c09c00523a3f1690ccbcb5b85f0192
djawny <djawny@op.pl>
2017-01-26 19:06:27 +0100

    zmiana 26/01

--- src/main/java/sda/zadanie22_json_unirest/HolidayApiClient.java
+++ src/main/java/sda/zadanie22_json_unirest/HolidayApiClient.java
@@ -9,21 +9,21 @@ public class HolidayApiClient {
     public static void main(String[] args) throws UnirestException {
         String country = "PL";
         String year = "2016";
+        String month = "01";
 
-        JsonNode jsonNode = Unirest.get("http://holidayapi.com/v1/holidays")
+        JsonNode jsonNode = Unirest.get("https://holidayapi.com/v1/holidays")
                 .queryString("key", "1f1c9c23-e6b1-4b71-8a06-b3fe9b176bfb")
                 .queryString("country", country)
                 .queryString("year", year)
+                .queryString("month", month)
                 .asJson()
                 .getBody();
-
+        System.out.println(jsonNode);
         String holiday = jsonNode.getObject()
                 .optJSONArray("holidays")
                 .getJSONObject(0)
                 .optString("name");
 
         System.out.println(holiday);
-
-
     }
 }
