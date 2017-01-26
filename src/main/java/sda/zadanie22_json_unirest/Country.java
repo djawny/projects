ff547aa1a4d751f65c1cd005bd53e9bc12502209
djawny <djawny@op.pl>
2017-01-26 19:59:44 +0100

    zmiana 26/01

--- src/main/java/sda/zadanie22_json_unirest/Country.java
+++ src/main/java/sda/zadanie22_json_unirest/Country.java
@@ -3,6 +3,8 @@ package sda.zadanie22_json_unirest;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,17 +16,13 @@ public class Country {
                 .header("Accept", "application/json")
                 .asJson().getBody();
 
-        List<String> capitals = new ArrayList<>();
+        JSONArray responseArray = response.getArray();
+        int size = responseArray.length();
+        for (int i = 0; i < size; i++) {
+            JSONObject jsonObject = responseArray.getJSONObject(i);
+            String country = jsonObject.optString("name");
+            String capital = jsonObject.optString("capital");
 
-        for (int i = 0; i < 8; i++) {
-            String capital = response
-                    .getArray()
-                    .getJSONObject(i)
-                    .optString("capital");
-            capitals.add(capital);
-        }
-
-        for (String capital : capitals) {
             JsonNode jsonNode = Unirest.get("http://api.openweathermap.org/data/2.5/weather")
                     .queryString("q", capital)
                     .queryString("appid", "779bcb1c99f4dcd8ffe6b596d5dc919d")
@@ -32,10 +30,13 @@ public class Country {
                     .asJson()
                     .getBody();
 
-            String temp = jsonNode.getObject()
-                    .optJSONObject("main")
-                    .optString("temp");
-            System.out.println(capital + ": " + temp + " C");
+            if (!capital.isEmpty()) {
+                String temp = jsonNode
+                        .getObject()
+                        .optJSONObject("main")
+                        .optString("temp");
+                System.out.println(country + " - " + capital + ": " + temp + " C");
+            }
         }
     }
 }
