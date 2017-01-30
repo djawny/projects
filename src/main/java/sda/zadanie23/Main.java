ccb79f9ff6382eb8c9d67feeb4a1d339c7c0549d
djawny <djawny@op.pl>
2017-01-30 17:52:20 +0100

    zmiana 26/01

--- src/main/java/sda/zadanie23/Main.java
+++
@@ -1,7 +0,0 @@
-package sda.zadanie23;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/main/java/sda/zadanie23_factorypatterns_designpatterns_singleton_builder/CarMain.java
@@ -0,0 +1,22 @@
+package sda.zadanie23_factorypatterns_designpatterns_singleton_builder;
+
+/*
+creational
+-factory pattern
+-abstract factory
+-singleton
+-factory
+structual
+-decorator
+-flyweight
+-adapter
+behavioral
+
+
+
+*/
+public class CarMain {
+    public static void main(String[] args) {
+        Car car = new Car.Builder("123").withBrand("Opel").withModel("Vectra").build();
+    }
+}
