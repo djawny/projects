717dbccbaf33b3e06fc646cbb42a1815a285c2ff
djawny <djawny@op.pl>
2017-01-30 19:22:05 +0100

    zmiana 26/01

--- src/main/java/sda/zadanie23_patterns/CarMain.java
+++ src/main/java/sda/zadanie23_patterns/CarMain.java
@@ -1,5 +1,12 @@
 package sda.zadanie23_patterns;
 
+//https://mvnrepository.com/
+//https://technologyconversations.com/2014/11/04/java-8-streams-micro-katas/
+//http://winterbe.com/posts/2014/07/31/java8-stream-tutorial-examples/
+//http://ggoralski.pl/
+//https://market.mashape.com/undefined
+//http://start.spring.io/
+
 /*
 Patterns:
 Creational:
