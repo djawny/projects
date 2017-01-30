607b9d9d66c0b31c4fd00368422e781288fccb7a
djawny <djawny@op.pl>
2017-01-30 19:23:07 +0100

    zmiana 26/01

--- src/main/java/sda/zadanie23_patterns/CarMain.java
+++ src/main/java/sda/zadanie23_patterns/CarMain.java
@@ -1,29 +1,5 @@
 package sda.zadanie23_patterns;
 
-//https://mvnrepository.com/
-//https://technologyconversations.com/2014/11/04/java-8-streams-micro-katas/
-//http://winterbe.com/posts/2014/07/31/java8-stream-tutorial-examples/
-//http://ggoralski.pl/
-//https://market.mashape.com/undefined
-//http://start.spring.io/
-
-/*
-Patterns:
-Creational:
-- factory pattern
-- abstract factory
-- singleton
-- factory
-Structural:
-- decorator
-- flyweight
-- adapter
-Behavioral:
-- strategy
-- null object
-- observer
-*/
-
 public class CarMain {
     public static void main(String[] args) {
         Car car = new Car.Builder("123").withBrand("Opel").withModel("Vectra").build();
---
+++ src/main/java/sda/zadanie23_patterns/Main.java
@@ -0,0 +1,31 @@
+package sda.zadanie23_patterns;
+
+//https://mvnrepository.com/
+//https://technologyconversations.com/2014/11/04/java-8-streams-micro-katas/
+//http://winterbe.com/posts/2014/07/31/java8-stream-tutorial-examples/
+//http://ggoralski.pl/
+//https://market.mashape.com/undefined
+//http://start.spring.io/
+
+/*
+Patterns:
+Creational:
+- factory pattern
+- abstract factory
+- singleton
+- factory
+Structural:
+- decorator
+- flyweight
+- adapter
+Behavioral:
+- strategy
+- null object
+- observer
+*/
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
