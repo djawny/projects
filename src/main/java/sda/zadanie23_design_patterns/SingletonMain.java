c8a377344d2665c521093ce4834b52e71119a358
djawny <djawny@op.pl>
2017-02-06 00:37:57 +0100

    zmiana

---
+++ src/main/java/sda/zadanie23_design_patterns/SingletonMain.java
@@ -0,0 +1,7 @@
+package sda.zadanie23_design_patterns;
+
+public class SingletonMain {
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance();
+    }
+}
